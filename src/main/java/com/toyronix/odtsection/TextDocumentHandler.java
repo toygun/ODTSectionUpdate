e98f0518cbce5a7922ce8734541f9c6798da389a
Toygun Özbek <toygun@web.de>
2012-06-06 18:04:32 +0200

    methods for embedded sections have been implemented

--- src/main/java/com/toyronix/odtsection/TextDocumentHandler.java
+++ src/main/java/com/toyronix/odtsection/TextDocumentHandler.java
@@ -1,6 +1,14 @@
 package com.toyronix.odtsection;
 
 import org.odftoolkit.simple.TextDocument;
+import org.odftoolkit.simple.text.Section;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TextDocumentHandler {
     TextDocument textDocument;
@@ -9,8 +17,25 @@ public class TextDocumentHandler {
         this.textDocument = textDocumentToHandle;
     }
 
-    public boolean hasEmbeddedSections() {
-        //TODO implement the method
+    public TextDocument getTextDocument() {
+        return textDocument;
+    }
+
+    public boolean hasEmbeddedSections() throws Exception {
+        int lengthOfSectionSourceTags = getTextDocument().getContentDom().getElementsByTagName("text:section-source").getLength();
+
+        if (lengthOfSectionSourceTags > 0)
+            return true;
+
         return false;
     }
+
+    public NodeList embeddedSectionList() throws Exception {
+        NodeList elementsByTagName = null;
+        if (hasEmbeddedSections()) {
+            elementsByTagName = getTextDocument().getContentDom().getElementsByTagName("text:section-source");
+        }
+        return elementsByTagName;
+    }
+
 }
